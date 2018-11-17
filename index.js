63638e6efa1832c0f44080093dfba849941cca25
zaid85j <zaid85j@gmail.com>
2018-11-17 12:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function calculatesFarePrice (start, destination) {
   } else if (distance > 400 && distance <= 2000) {
     return 0.02 * distance;
   } else if (distance > 2000 && distance <= 2500) {
-    return 25 ;
+    return 25 * distance ;
   } else {
     return 'cannot travel that far';
   }
