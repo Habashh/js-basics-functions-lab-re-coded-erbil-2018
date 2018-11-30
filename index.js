6fa66f62a9e2bbb3530793d8b42e99120823d468
zaid85j <zaid85j@gmail.com>
2018-11-30 16:23:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-function distanceFromHqInBlocks (blockNumber) {
-  if (blockNumber > 42) {
-    return blockNumber - 42;
-  } else {
-    return 42 - blockNumber;
+function distanceFromHqInBlocks (blocksNo) {
+  if (blocksNo>42) 
+  {
+    return blocksNo-42
+       } else 
+  
+  {
+    return 42-blocksNo
+  }
+    }
+
+function distanceFromHqInFeet (blocksNo) {
+  //we call here the first function to pass the blocksNo and *264
+  return distanceFromHqInBlocks(blocksNo)*264;
+}
+//here we calculate distance uptown/downtown, not from river to river.
+function distanceTravelledInFeet (fromPickup,toHq) {
+  if (fromPickup< toHq)
+  {
+    return (toHq- fromPickup )*264;
+  } else
+  {
+    return (fromPickup  -toHq)*264;
   }
 }
+//distance 400 and 2000 feet price is 2 cent (400 are free!)2000 and 2500 25 Dollar.over 2500 cannot travel that far
+function calculatesFarePrice (fromPickup, toHq) {
+  const disInFoot = distanceTravelledInFeet(fromPickup, toHq)
 
-function distanceFromHqInFeet (blockNumber) {
-  return distanceFromHqInBlocks(blockNumber) * 264;
-}
-
-function distanceTravelledInFeet (start, destination) {
-  if (start < destination) {
-    return (destination - start) * 264;
-  } else {
-    return (start - destination) * 264;
-  }
-}
-
-function calculatesFarePrice (start, destination) {
-  const distance = distanceTravelledInFeet(start, destination);
-
-  if (distance <= 400) {
-    return 0;
-  } else if (distance > 400 && distance <= 2000) {
-    return .02 * distance;
-  } else if (distance > 2000 && distance < 2500) {
+  if (disInFoot <= 400) {
+    return disInFoot* 0;
+  } 
+  else if (disInFoot > 400 && disInFoot <= 2000)
+  {
+    
+  return 0.02 * disInFoot;
+  } 
+  else if (disInFoot > 2000 && disInFoot < 2500) 
+  {
     return 25;
-  } else {
-    return 'cannot travel that far';
+  }
+  else
+  {
+    return "cannot travel that far";
   }
 }
